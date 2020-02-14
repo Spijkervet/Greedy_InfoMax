c7f37ef9a7044715708b0a84ea58155e19aa681e
Janne Spijkervet <janne.spijkervet@gmail.com>
2020-02-14 11:54:49 +0100

    Changed vision train file parameters to match paper's

--- vision_traineval.sh
+++ vision_traineval.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Training the Greedy InfoMax Model on vision data (stl-10)"
-python -m GreedyInfoMax.vision.main_vision --download_dataset --save_dir vision_experiment
+python -m GreedyInfoMax.vision.main_vision --download_dataset --batch_size 16 --learning_rate 1.5e-4 --num_epochs 300 --save_dir vision_experiment
 
 echo "Testing the Greedy InfoMax Model for image classification"
 python -m GreedyInfoMax.vision.downstream_classification --model_path ./logs/vision_experiment --model_num 299
